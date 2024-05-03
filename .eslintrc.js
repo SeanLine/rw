2a10b83014b88c7453cbbf2d28134e5501f4d0d5
穆森 <mus@qtrade.com,cn>
2024-05-03 22:15:25 +0800

    feat: eslint 解析基础

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,19 @@
-
 module.exports = {
   root: true,
   parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      modules: true,
-    },
-  },
   plugins: ['import'],
   rules: {
+    'import/no-unresolved': 2,
     'max-lines': ['error', { max: 2 }],
   },
   settings: {
     'import/resolver': {
       typescript: {
-        project: ["tsconfig.json"],
+        project: ['tsconfig.json'],
         extensions: ['.tsx', '.ts', '.js', '.jsx'],
       },
       node: {
-        project: ["tsconfig.json"],
+        project: ['tsconfig.json'],
         extensions: ['.tsx', '.ts', '.js', '.jsx'],
       },
     },
